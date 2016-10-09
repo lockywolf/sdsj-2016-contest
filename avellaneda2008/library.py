cb7855fcc222e92f629425c02a74b787d065341f
lockywolf <lockywolf@gmail.com>
2016-10-09 15:25:19 +0300

    First Commit. Creating the basic structure. Fixing VCS error.
    Signed-off-by: lockywolf <lockywolf@gmail.com>

---
+++ avellaneda2008/library.py
@@ -0,0 +1,14 @@
+# coding=utf-8
+u"""This file has a set of library functions for the basic FinMath exercise."""
+
+
+class FinMath(object):
+    u"""This class is to learn finmath, time series analysis and stochastic PDEs."""
+    pass  # Doesn't do anything, but reminds the syntax.
+
+    def __init__(self):
+        print("library.py: mylibraryfunctionobject init: my name is " + __name__)
+
+if __name__ == "__main__":
+    print("I am from the avellaneda2008 package, but I am executed as a script and my name is: " + __name__)
+
---
+++ myRun.py
@@ -0,0 +1,3 @@
+from avellaneda2008 import library
+
+print("myrun.py: my name is " + __name__)
