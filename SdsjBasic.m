47c2145dc5ecc7128b687cd2696bac1e8bc27b81
lockywolf <lockywolf@gmail.com>
2016-10-16 11:16:06 +0300

    Preparing to delete.

---
+++ SdsjBasic.m
@@ -0,0 +1,18 @@
+classdef SdsjBasic
+	%SDSJ_BASIC basic class for Sberbank Data Science Journey
+	%   Detailed explanation goes here
+	
+	properties
+		Transactions
+	end
+	
+	methods
+		function obj = SdsjBasic()
+			fprintf('hello, world\n');
+			%obj.Transactions = readtable( 'transactions.csv', 'Delimiter', ',' );
+		end
+	end
+	
+end
+
+
--- sdsj_runner.m
+++ sdsj_runner.m
@@ -1,1 +1,1 @@
-A = sdsj_basic()
+A = SdsjBasic
